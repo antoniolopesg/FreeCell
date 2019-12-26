bedbe039e63ef4c53e580f65ca3fdcf45cb1734b
Antonio Lopes <antoniolopesg@protonmail.com>
2019-12-26 19:39:11 -0300

    some necessary operations with the card class

--- src/classes/Card.java
+++ src/classes/Card.java
@@ -1,21 +1,41 @@
 package classes;
 
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Card {
-    private String suit;
+    public static final int CLUBS = 1;
+    public static final int DIAMONDS = 2;
+    public static final int HEARTS = 3;
+    public static final int SPADES = 4;
+
+    private int suit;
     private String value;
     private String imgPath;
+    private ImageView container;
 
-    public Card(String suit, String value, String imgPath){
+    public Card(int suit, String value){
         this.setSuit(suit);
         this.setValue(value);
-        this.setImgPath(imgPath);
+        this.setImgPath(suit, value);
     }
 
-    public String getSuit() {
+    public void setContainer() {
+        this.container = new ImageView(new Image(this.imgPath));
+    }
+
+    public ImageView getContainer() {
+        return container;
+    }
+
+    public int getSuit() {
         return suit;
     }
 
-    public void setSuit(String suit) {
+    public void setSuit(int suit) {
         this.suit = suit;
     }
 
@@ -31,7 +51,33 @@ public class Card {
         return imgPath;
     }
 
-    public void setImgPath(String imgPath) {
-        this.imgPath = imgPath;
+    public void setImgPath(int suit, String value) {
+        String path = null;
+
+        switch (suit){
+            case CLUBS:
+                path = "src/cards/clubs/" + this.getValue() + ".png";
+                break;
+            case DIAMONDS:
+                path = "src/cards/diamonds/" + this.getValue() + ".png";
+                break;
+            case HEARTS:
+                path = "src/cards/hearts/" + this.getValue() + ".png";
+                break;
+            default:
+                path = "src/cards/spades/" + this.getValue() + ".png";
+        }
+
+        this.imgPath = path;
+    }
+
+    public static ArrayList <Card> loadSuit(int suit){
+        ArrayList <Card> suitCards = new ArrayList <Card>();
+
+        for(int i = 0; i < 13; i++){
+            suitCards.add(new Card(suit, Integer.toString(i + 1)));
+        }
+
+        return suitCards;
     }
 }
